29f0b05d82aff11aa67f7b6d38b33f61fcb97d54
TaviaCL <82876442+TaviaCL@users.noreply.github.com>
2021-05-20 19:21:24 +0200

    Create project_test.py

---
+++ project_test.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from project import Player
+
+
+class Testplayer(TestCase):
+
+    def setUp(self):
+        self.p = Player("toto")
+
+    def test_set_age(self):
+        self.p.setAge(10)
+        self.assertEqual(self.p.age, 10)
+        self.p.setAge(15)
+        self.assertEqual(self.p.age, 15)
